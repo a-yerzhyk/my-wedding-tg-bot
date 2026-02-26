2196c2e8e687f08aa66408f9e1b7fda62f061a32
Andrii Yerzhykevych <andriiyerzh@gmail.com>
2026-02-26 15:18:16 +0200

    fix: dont set header color manually; exit fullscreen on desktop

--- src/services/telegram-sdk/index.ts
+++ src/services/telegram-sdk/index.ts
@@ -4,25 +4,23 @@ import {
   init,
   miniApp,
   postEvent,
-  // swipeBehavior,
   viewport
 } from '@tma.js/sdk'
-
-const MAIN_HEADER_COLOR_RGB = '#FEF9F5'
 
 export async function initSDK() {
   init()
   backButton.isSupported() && backButton.mount()
-  miniApp.mount(), miniApp.setHeaderColor(MAIN_HEADER_COLOR_RGB)
+  miniApp.mount()
   viewport.mount().then(() => {
     viewport.expand()
     viewport.bindCssVars()
     viewport.isFullscreen()
     if (!viewport.isFullscreen() && viewport.requestFullscreen.isAvailable() && isMobile()) {
       viewport.requestFullscreen()
+    } else {
+      viewport.exitFullscreen()
     }
   })
-  // swipeBehavior.isSupported() && (swipeBehavior.mount(), swipeBehavior.disableVertical())
 
   postEvent('web_app_toggle_orientation_lock', { locked: true })
 }
