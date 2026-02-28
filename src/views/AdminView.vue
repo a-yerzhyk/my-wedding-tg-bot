a9497f70abc562150ff233462cf4088d27fbe461
Andrii Yerzhykevych <andriiyerzh@gmail.com>
2026-03-01 00:55:08 +0200

    feat: list guest requests

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,4 +1,33 @@
 <script setup lang="ts">
+import { useUserStore } from '@/stores/user';
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import { ref } from 'vue';
+import { getApiGuestsRequests } from '@/services/client';
+import type { GuestUser } from '@/services/types'
+import { openTelegramLink } from '@tma.js/sdk'
+
+const router = useRouter()
+const userStore = useUserStore()
+
+const usersList = ref<GuestUser[]>([])
+
+function openProfile(username: string) {
+  openTelegramLink(`https://t.me/${username}`)
+}
+
+onMounted(() => {
+  if (!userStore.isAdmin) {
+    router.push('/')
+  }
+
+  getApiGuestsRequests().then((response) => {
+    const { data } = response
+    if (data) {
+      usersList.value = data
+    }
+  })
+})
 </script>
 
 <template>
@@ -8,10 +37,22 @@
         <p class="menu-view__subheading text-secondary">Панель адміна</p>
         <p class="menu-view__heading text-header">Список запитів</p>
       </div>
-      <div class="p-3 space-y-3">
-        <div class="guest-request__item bg-white rounded p-2">
-          <p>Марія Андрій</p>
-          <p>maria.andriy@gmail.com</p>
+      <div class="p-3 space-y-3" v-if="usersList.length">
+        <div
+          v-for="user in usersList"
+          :key="user.id"
+          class="guest-request__item bg-white rounded p-2"
+        >
+          <div class="space-y-1">
+            <p>{{ user.firstName }} {{ user.lastName }}</p>
+            <p
+              class="link"
+              @click="() => user.username && openProfile(user.username)"
+            >
+              @{{ user.username }}
+            </p>
+          </div>
+          <p>{{ user.approvalStatus }}</p>
         </div>
       </div>
     </div>
@@ -29,8 +70,9 @@
     border-radius: 16px;
     padding: 16px;
     display: flex;
+    align-items: center;
+    justify-content: space-between;
     gap: 14px;
-    align-items: flex-start;
   }
 }
 </style>
