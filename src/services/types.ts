c0e8dd6c31d5489a5b557c36f473af17f38c0a0d
Andrii Yerzhykevych <andriiyerzh@gmail.com>
2026-03-01 00:42:59 +0200

    feat: create developer friendly types

---
+++ src/services/types.ts
@@ -0,0 +1,19 @@
+import type {
+  GetApiGuestsRequestsResponse,
+  GetApiGalleryResponse,
+  GetApiGalleryByGalleryIdResponse,
+  PostApiAuthTelegramResponses
+} from './client'
+
+// Auth
+export type AuthResponse = PostApiAuthTelegramResponses[200]
+export type AuthUser = AuthResponse['user']
+
+// Guests
+export type GuestUser = GetApiGuestsRequestsResponse extends Array<infer T> ? T : never
+export type ApprovalStatus = GuestUser['status']
+
+// Gallery
+export type Gallery = GetApiGalleryResponse extends Array<infer T> ? T : never
+export type GalleryDetail = GetApiGalleryByGalleryIdResponse
+export type Photo = GalleryDetail['photos'] extends Array<infer T> ? T : never
