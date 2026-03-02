0dcc45c010309124708ae8f9036a2e2f147ebb52
Andrii Yerzhykevych <andriiyerzh@gmail.com>
2026-03-03 01:27:14 +0200

    feat: galleries list (admins only)

---
+++ src/components/GalleryItem.vue
@@ -0,0 +1,104 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+  name: string;
+  username?: string;
+  avatarUrl?: string;
+  previews: string[];
+  photoCount: number;
+}>(), {
+  avatarUrl: '',
+  name: '',
+  username: '',
+  previews: () => []
+})
+</script>
+
+<template>
+  <div class="gallery-item">
+    <div class="gallery-item__header">
+      <img :src="avatarUrl" alt="Avatar" class="gallery-item__avatar">
+      <div class="gallery-item__user">
+        <div class="gallery-item__name text-primary">{{ name }}</div>
+        <div v-if="username" class="gallery-item__username text-secondary link">
+          @{{ username }}
+        </div>
+      </div>
+      <div class="text-primary text-sm">
+        {{ photoCount }}
+      </div>
+    </div>
+    <div class="gallery-item__divider"></div>
+    <div class="gallery-item__previews">
+      <img
+        v-for="preview in previews"
+        :key="preview"
+        class="gallery-item__preview"
+        :src="preview"
+        alt="Preview"
+      />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.gallery-item {
+  width: 100%;
+  background: var(--white);
+  border-radius: 14px;
+  padding: 14px 16px;
+  box-shadow: var(--shadow-soft);
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+
+  &__header {
+    display: flex;
+    align-items: center;
+    gap: 12px;
+  }
+
+  &__avatar {
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    object-fit: cover;
+  }
+
+  &__user {
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+    flex: 1;
+  }
+
+  &__name {
+    font-size: 1rem;
+  }
+
+  &__username {
+    font-size: 0.7rem;
+    text-transform: lowercase;
+    color: var(--dusty)
+  }
+
+  &__divider {
+    width: 100%;
+    height: 1px;
+    background: linear-gradient(to right, transparent , var(--dusty) 10%, var(--dusty) 90%, transparent);
+  }
+
+  &__previews {
+    display: flex;
+    height: 100px;
+    gap: 8px;
+    padding: 4px 0;
+  }
+
+  &__preview {
+    flex: 1;
+    height: 100%;
+    border-radius: 8px;
+    object-fit: cover;
+  }
+}
+</style>
--- src/views/PhotosView.vue
+++ src/views/PhotosView.vue
@@ -2,39 +2,74 @@
 import VButton from '@/components/VButton.vue';
 import { useUserStore } from '@/stores/user';
 import { openProfile } from '@/services/tma-sdk';
+import { onMounted, ref } from 'vue';
+import { getApiGallery } from '@/services/client';
+import type { Gallery } from '@/services/types';
+import GalleryItem from '@/components/GalleryItem.vue';
 
 const userStore = useUserStore()
+const galleries = ref<Gallery[]>([])
+
+async function loadGalleries() {
+  const res = await getApiGallery()
+  if (res.data) {
+    galleries.value = res.data
+  }
+}
+
+onMounted(async () => {
+  if (userStore.isAdmin) {
+    await loadGalleries()
+  }
+})
 </script>
 
 <template>
   <div class="photos-view">
-    <div class="menu-view__content pl-[16px] pr-[16px]">
-      <Transition name="fade" mode="out-in">
-        <div class="text-header" v-if="userStore.isApproved">
-          У розробці...
-        </div>
-        <div class="text-header text-center" v-else-if="userStore.isPending">
-          Ваша заявка розглядається.<br/>
-          Для пришвидшення процесу розгляду зверніться до винуватців цього дійства :)<br/>
-          <div class="flex justify-center gap-2 text-lg">
-            <p class="link" @click="() => openProfile('andriiyer')">
-              @andriiyer
-            </p>
-            <p class="link" @click="() => openProfile('Mariia_bittersweet')">
-              @Mariia_bittersweet
-            </p>
+    <div class="menu-view__content ">
+      <div class="menu-view__header" v-if="userStore.isAdmin">
+        <p class="menu-view__subheading text-secondary">хайлайти</p>
+        <p class="menu-view__heading text-header">Галерея</p>
+      </div>
+      <div class="space-y-3 p-4 pt-2" v-if="userStore.isAdmin">
+        <GalleryItem
+          v-for="gallery in galleries"
+          :key="gallery.id"
+          :avatarUrl="gallery.user.avatarUrl"
+          :name="gallery.user.firstName"
+          :username="gallery.user.username"
+          :previews="gallery.previews"
+          :photoCount="gallery.photoCount"
+        />
+      </div>
+      <div v-if="!userStore.isAdmin" class="w-full h-full flex justify-center items-center pl-[16px] pr-[16px]">
+        <Transition name="fade" mode="out-in">
+          <div class="text-header" v-if="!userStore.isAdmin && userStore.isApproved">
+            У розробці...
           </div>
-        </div>
-        <p class="text-header" v-else-if="userStore.isDenied">
-          Ваша заявка відхилена
-        </p>
-        <div class="flex flex-col items-center text-header text-xl gap-2" v-else-if="!userStore.isApproved">
-          <p class="text-center">Для перегляду та завантаження фото вам потрібно відправити заявку</p>
-          <VButton type="success" @click="userStore.submitGuestRequest">
-            Прийняти запрошення
-          </VButton>
-        </div>
-      </Transition>
+          <div class="text-header text-center" v-else-if="userStore.isPending">
+            Ваша заявка розглядається.<br/>
+            Для пришвидшення процесу розгляду зверніться до винуватців цього дійства :)<br/>
+            <div class="flex justify-center gap-2 text-lg">
+              <p class="link" @click="() => openProfile('andriiyer')">
+                @andriiyer
+              </p>
+              <p class="link" @click="() => openProfile('Mariia_bittersweet')">
+                @Mariia_bittersweet
+              </p>
+            </div>
+          </div>
+          <p class="text-header" v-else-if="userStore.isDenied">
+            Ваша заявка відхилена
+          </p>
+          <div class="flex flex-col items-center text-header text-xl gap-2" v-else-if="!userStore.isApproved">
+            <p class="text-center">Для перегляду та завантаження фото вам потрібно відправити заявку</p>
+            <VButton type="success" @click="userStore.submitGuestRequest">
+              Прийняти запрошення
+            </VButton>
+          </div>
+        </Transition>
+      </div>
     </div>
   </div>
 </template>
@@ -44,9 +79,6 @@ const userStore = useUserStore()
   background: var(--background-3);
 
   .menu-view__content {
-    display: flex;
-    align-items: center;
-    justify-content: center;
     font-size: 1.5rem;
   }
 }
